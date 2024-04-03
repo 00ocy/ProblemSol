144d5585232a105bbdf060e42aa641290bf586b1
OCY <ok0523ok@gmail.com>
2024-04-04 07:56:32 +0900

    [OCY] 메시로 별기둥 만들기

---
+++ ProbblemSol/Assets/2. Scripts/StaticMeshGen.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(StaticMeshGen))]
+public class StaticMeshGenEditor : Editor
+{
+    //버튼만들기 예제
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        StaticMeshGen script = (StaticMeshGen)target;
+
+        if (GUILayout.Button("Generate Mesh"))
+        {
+            script.GenerateMesh();
+        }
+
+    }
+}
+
+//메쉬만들기 예제
+public class StaticMeshGen : MonoBehaviour
+{
+    public void GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[]
+        {
+            // 아래 큰오각형
+            new Vector3 (0.0f, 0.0f, 1.1f),      // 위        0
+            new Vector3 (1.1f, 0.0f, 0.33f),     // 우 상단   1
+            new Vector3 (0.55f, 0.0f, -0.88f),   // 우 하단   2
+            new Vector3 (-0.55f, 0.0f, -0.88f),  // 좌 하단   3
+            new Vector3 (-1.1f, 0.0f, 0.33f),    // 좌 상단   4
+
+            // 아래 작은오각형
+            new Vector3 (0.0f, 0.0f, -0.5f),     // 아래      5
+            new Vector3 (-0.5f, 0.0f, -0.15f),   // 좌 하단   6
+            new Vector3 (-0.25f, 0.0f, 0.4f),    // 좌 상단   7
+            new Vector3 (0.25f, 0.0f, 0.4f),     // 우 상단   8
+            new Vector3 (0.5f, 0.0f, -0.15f),    // 우 하단   9
+
+
+            // 위 큰오각형
+            new Vector3 (0.0f, 2.0f, 1.1f),      // 위        10
+            new Vector3 (1.1f, 2.0f, 0.33f),     // 우 상단   11
+            new Vector3 (0.55f, 2.0f, -0.88f),   // 우 하단   12
+            new Vector3 (-0.55f, 2.0f, -0.88f),  // 좌 하단   13
+            new Vector3 (-1.1f, 2.0f, 0.33f),    // 좌 상단   14
+
+            // 위 작은오각형
+            new Vector3 (0.0f, 2.0f, -0.5f),     // 아래      15
+            new Vector3 (-0.5f, 2.0f, -0.15f),   // 좌 하단   16
+            new Vector3 (-0.25f, 2.0f, 0.4f),    // 좌 상단   17
+            new Vector3 (0.25f, 2.0f, 0.4f),     // 우 상단   18
+            new Vector3 (0.5f, 2.0f, -0.15f),    // 우 하단   19
+        };
+
+        mesh.vertices = vertices;
+
+        int[] triangleIndices = new int[]
+        {
+            /*// 아래 큰오각형
+            0, 1, 2,  // 위 우상단 우하단
+            0, 2, 3,  // 위 우하단 좌하단
+            0, 3, 4,  // 위 좌하단 좌상단*/
+
+            // 아래 작은오각형
+            5, 7, 6,  // 아래 좌상단 좌하단 
+            5, 8, 7,  // 아래 우상단 좌상단 
+            5, 9, 8,  // 아래 우하단 우상단 
+        
+            // 아래 별
+            7, 8, 0,  // 작좌상단 작우상단 큰위 
+            8, 9, 1,  // 작우상단 작우하단 큰우상단 
+            9, 5, 2,  // 작우하단 작아래 큰우하단 
+            5, 6, 3,  // 작아래 작좌하단 큰좌하단 
+            6, 7, 4,  // 작좌하단 작좌상단 큰좌상단 
+       
+
+            /*// 위 큰오각형
+            10, 11, 12,  // 위 우상단 우하단
+            10, 12, 13,  // 위 우하단 좌하단
+            10, 13, 14,  // 위 좌하단 좌상단*/
+
+            // 위 작은오각형
+            15, 16, 17,  // 아래 좌하단 좌상단
+            15, 17, 18,  // 아래 좌상단 우상단
+            15, 18, 19,  // 아래 우상단 우하단
+        
+            // 위 별
+            17, 10, 18,  // 작좌상단 큰위 작우상단
+            18, 11, 19,  // 작우상단 큰우상단 작우하단
+            19, 12, 15,  // 작우하단 큰우하단 작아래
+            15, 13, 16,  // 작아래 큰좌하단 작좌하단
+            16, 14, 17,  // 작좌하단 큰좌상단 작좌상단
+            
+            // 기둥 
+            10, 17, 7,
+            10, 7, 0,
+
+            18, 10, 0,
+            18, 0, 8,
+
+            11, 18, 8,
+            11, 8, 1,
+
+            19, 11, 1,
+            19, 1, 9,
+            
+            12, 19, 9,
+            12, 9, 2,
+
+            15, 12, 2,
+            15, 2, 5,
+
+            13, 15, 5,
+            13, 5, 3,
+
+            16, 13, 3,
+            16, 3, 6,
+
+            14, 16, 6,
+            14, 6, 4,
+
+            17, 14, 4,
+            17, 4, 7,
+
+                
+        };
+
+        mesh.triangles = triangleIndices;
+
+        MeshFilter mf = gameObject.GetComponent<MeshFilter>();
+        MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
+
+        if (mf == null)
+            mf = gameObject.AddComponent<MeshFilter>();
+        if (mr == null)
+            mr = gameObject.AddComponent<MeshRenderer>();
+
+        mf.mesh = mesh;
+    }
+
+
+
+}   
